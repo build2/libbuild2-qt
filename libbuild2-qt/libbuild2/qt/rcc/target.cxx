1a86917e98b948713a826ceeb9187fd01bb6b23e
Francois Kritzinger <francois@codesynthesis.com>
2023-05-16 15:23:22 +0200

    Add target type

---
+++ libbuild2-qt/libbuild2/qt/rcc/target.cxx
@@ -0,0 +1,26 @@
+#include <libbuild2/qt/rcc/target.hxx>
+
+namespace build2
+{
+  namespace qt
+  {
+    namespace rcc
+    {
+      // @@ TMP extern? Ignored if there's an initializer.
+      //
+      extern const char qrc_ext[] = "qrc";
+      const target_type qrc::static_type
+      {
+        "qrc",
+        &file::static_type,
+        &target_factory<qrc>,
+        &target_extension_fix<qrc_ext>,
+        nullptr, /* default_extension */
+        &target_pattern_fix<qrc_ext>,
+        nullptr /* print */,
+        &file_search,
+        target_type::flag::none
+      };
+    }
+  }
+}
---
+++ libbuild2-qt/libbuild2/qt/rcc/target.hxx
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <libbuild2/types.hxx>
+
+#include <libbuild2/target.hxx>
+
+#include <libbuild2/qt/export.hxx>
+
+namespace build2
+{
+  namespace qt
+  {
+    namespace rcc
+    {
+      class LIBBUILD2_QT_SYMEXPORT qrc: public file
+      {
+      public:
+        qrc (context& c, dir_path d, dir_path o, string n)
+            : file (c, move (d), move (o), move (n))
+        {
+          dynamic_type = &static_type;
+        }
+
+      public:
+        static const target_type static_type;
+      };
+    }
+  }
+}
