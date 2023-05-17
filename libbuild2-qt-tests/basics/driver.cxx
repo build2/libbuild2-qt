32c68366e90e00c4a19e072a3289a50595c406b6
Francois Kritzinger <francois@codesynthesis.com>
2023-05-17 16:19:35 +0200

    Test RCC

--- libbuild2-qt-tests/basics/driver.cxx
+++ libbuild2-qt-tests/basics/driver.cxx
@@ -1,4 +1,18 @@
+#include <QtCore/QFile>
+#include <QtCore/QResource>
+
+#include "qrc_foo.hxx"
+
 int
 main ()
 {
+  QFile foo (":/foo.txt");
+  assert (foo.exists ());
+
+  QFile bar (":/bar.txt");
+  assert (bar.exists ());
+
+  assert (QResource::registerResource (OUT_BASE "/baz.rcc"));
+  QFile baz (":/baz.txt");
+  assert (baz.exists ());
 }
