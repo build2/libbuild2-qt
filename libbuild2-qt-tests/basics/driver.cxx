d6af19155eb7eabb409e231394dbdc4e12b41262
Francois Kritzinger <francois@codesynthesis.com>
2023-06-12 14:43:14 +0200

    Test RCC (dynamic resource dependency extraction)

--- libbuild2-qt-tests/basics/driver.cxx
+++ libbuild2-qt-tests/basics/driver.cxx
@@ -8,11 +8,18 @@ main ()
 {
   QFile foo (":/foo.txt");
   assert (foo.exists ());
+  QFile foo2 (":/foo2.txt");
+  assert (foo.exists ());
+  QFile foo3 (":/foo3.txt");
+  assert (foo.exists ());
 
   QFile bar (":/bar.txt");
   assert (bar.exists ());
 
   assert (QResource::registerResource (OUT_BASE"baz.rcc"));
-  QFile baz (":/baz.txt");
-  assert (baz.exists ());
+  {
+    QFile baz (":/baz.txt");
+    assert (baz.exists ());
+  }
+  assert (QResource::unregisterResource (OUT_BASE"baz.rcc"));
 }
