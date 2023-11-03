5c8e0b243c0f933b95e0b3a2759b777b246b8e54
Francois Kritzinger <francois@codesynthesis.com>
2023-11-03 08:58:46 +0200

    Automoc moc macro scan tests

--- libbuild2-qt-tests/automoc/source/source.hxx
+++ libbuild2-qt-tests/automoc/source/source.hxx
@@ -4,7 +4,12 @@
 
 class Source: public QObject
 {
-  Q_OBJECT
+  // Add things around the Q_OBJECT macro to test the automoc rule for false
+  // negatives. (See sink.hxx for the conventional case and nomoc.hxx.in for
+  // the false positive tests.)
+  //
+
+  /* */Q_OBJECT;/**/ //
 
 signals:
   // Undefined reference errors during link mean QtCore's macros weren't
